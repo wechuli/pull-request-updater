878a741a013e0895c7c433b2500d17610b86b9a8
wechuli <wechulipaul@yahoo.com>
2023-02-01 18:44:39 +0300

    log error if invalid repository is supplied

--- src/utils/extractor.js
+++ src/utils/extractor.js
@@ -2,8 +2,19 @@ const core = require("@actions/core");
 
 function extractInputsAndEnvs() {
   const token = core.getInput("token");
-  const repoFull = process.env["GITHUB_REPOSITORY"];
-  const [owner, repo] = repoFull.split("/");
+  const repoFull = core.getInput("repo") || process.env["GITHUB_REPOSITORY"];
+
+  let owner, repo;
+
+  // check if the repository is valid
+  try {
+    [owner, repo] = repoFull.split("/");
+  } catch (error) {
+    throw new Error(
+      "It appears the repository format is invalid. Please use the format 'owner/repo'."
+    );
+  }
+
   return [token, owner, repo];
 }
 
