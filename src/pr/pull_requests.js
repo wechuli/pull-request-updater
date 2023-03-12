6fc285fd881ec1eab347bb5372dbb15824c41a13
wechuli <wechulipaul@yahoo.com>
2023-03-12 11:21:50 +0300

    correctly parse the filtered prs

--- src/pr/pull_requests.js
+++ src/pr/pull_requests.js
@@ -39,9 +39,8 @@ class PullRequests {
         if (pull_request["behind_by"] > 0) {
           filteredPRs.push(pull_request);
         }
+        this.filteredPulls = pr;
       }
-
-      this.filteredPulls = pr;
     } catch (error) {
       console.log(error);
     }
