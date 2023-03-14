3776fe9fb0de8a75b9a82bfad402a2e112f81f91
wechuli <wechulipaul@yahoo.com>
2023-03-14 10:35:59 +0300

    add ability to choose whether to create comments on the pull requests or not

--- src/utils/branchesFilter.js
+++ src/utils/branchesFilter.js
@@ -1,7 +1,9 @@
 function branchesFilter(branches) {
-
-    filteredbranches
+  let filteredbranches = [];
+  //   remove empty spaces
+  branches = branches.replace(/\s/g, "");
   // check if the string contains a comma
+
   if (branches.includes(",")) {
     // split the string into an array
     branches = branches.split(",");
--- src/utils/extractor.js
+++ src/utils/extractor.js
@@ -5,7 +5,8 @@ function extractInputsAndEnvs(
   repoFull = core.getInput("repo"),
   base = core.getInput("base"),
   head = core.getInput("head"),
-  updateforks = core.getInput("updateforks")
+  updateforks = core.getInput("updateforks"),
+  createComments = core.getInput("create-comments")
 ) {
   let reposplit = repoFull.split("/");
 
