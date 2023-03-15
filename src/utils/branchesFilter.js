9ca55614b6b0ec087e207ce2a7dc45df53197412
wechuli <wechulipaul@yahoo.com>
2023-03-15 22:21:55 +0300

    add logic for filtering by branches

--- src/utils/branchesFilter.js
+++ src/utils/branchesFilter.js
@@ -1,11 +1,20 @@
+const pm = require("picomatch");
+const core = require("@actions/core");
+
 function branchesFilter(branches) {
   let filteredbranches = [];
-  //   remove empty spaces
-  branches = branches.replace(/\s/g, "");
-  // check if the string contains a comma
-
   if (branches.includes(",")) {
-    // split the string into an array
-    branches = branches.split(",");
+    let branchesArray = branches.split(",");
+    for (let branch of branchesArray) {
+      filteredbranches.push(pm(branch));
+    }
+  } else {
+    filteredbranches.push(pm(branches));
   }
+
+  return filteredbranches;
 }
+
+module.exports = {
+  branchesFilter,
+};
