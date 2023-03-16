85f8e150bf4e106d12e91ed612099d1d0a593ae2
wechuli <wechuli@users.noreply.github.com>
2023-03-16 05:53:34 +0000

    Action build at commit 70996e158b73a32441796cf1dfc26310da7c7b11

--- dist/index.js
+++ dist/index.js
@@ -15269,7 +15269,6 @@ function extractInputsAndEnvs(
   createComments = core.getInput("create-comments")
 ) {
   let reposplit = repoFull.split("/");
-
   // check if the repository is valid
   try {
     if (reposplit.length !== 2) {
